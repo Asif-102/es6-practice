9be51da69a17d15229a0bff61bd90c2507c8beb7
Asif2535 <aiqbal517@gmail.com>
2021-02-01 19:19:06 +0600

    some practice

---
+++ letconst.js
@@ -0,0 +1,10 @@
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+console.log(numbers);
+
+const nayok = { name: "sakib khan", phone: 4578 };
+
+let patienName = "Rahim Chacha";
+patienName = "Fatema Khala";
+console.log(patienName);
---
+++ template.js
@@ -0,0 +1,18 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " is a good boy.";
+const fullName2 = `${firstName} ${lastName} is a good boy.`;
+console.log(fullName);
+console.log(fullName2);
+const fullName3 = `${firstName} ${20 + 50 + 30} is a good boy.`;
+console.log(fullName3);
+
+const multiLine = "I love cooding\n"
+    + "I miss myself\n"
+    + "I need practice"
+const multiLine2 = `I love cooding
+I miss myself
+I need practice`
+
+console.log(multiLine);
+console.log(multiLine2);
