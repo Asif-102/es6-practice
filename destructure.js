fa6bc3004abc2f840b8ed8053f5199b457632cc2
Asif2535 <aiqbal517@gmail.com>
2021-02-02 01:44:35 +0600

    destructure-object-array-destructure-complex-object

---
+++ destructure.js
@@ -0,0 +1,30 @@
+const person = {name: 'Jack Willian', age:17, job:'facebooker', gfName:'Ema Watson', address:'Kocu Khet', phone:'01454356', friends:['Tom Hanks', 'Tom Cruise', 'Tom yarn']}
+
+// const gfName = person.gfName;
+// const phone = person.phone;
+const {phone, gfName, address, salary} = person; //object destructure
+
+console.log(gfName, phone ,salary, address);
+console.log(gfName, phone);
+console.log(gfName, phone);
+console.log(gfName, phone);
+
+const complexObject = {
+    name  : 'abc',             // <== nested object
+    info :{
+        address: 'kola bagan',
+        leader : 'Tiger Leader'
+    }
+}
+
+const {leader} = complexObject.info;
+
+console.log(leader);
+
+
+const friends = ['Sakib Khan', 'Arman Khan', 'Amir Khan','Arman Khan', 'Salman Khan', 'sharukh khan'];
+
+const [chotoFriend, nextFriend,...restFriends] = friends; //array desctructure
+
+console.log(chotoFriend, nextFriend);
+console.log(restFriends);
